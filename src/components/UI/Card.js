5fa28b0ffad7a9a43c8116b44d4572d265c0c885
TonmoyGoon <onmoygoon@gmail.com>
2022-05-07 11:25:37 +0600

    updating

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,7 @@
 import classes from "./Card.module.css";
 
 const Card = props => {
-    return <div className={classes.card}>{props.children}</div>
+    return <div className={`${classes.card} ${props.className}`}>{props.children}</div>;  // As Card is our custom component so we have to define it by props.className
 }
 
 export default Card;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,22 @@
-import "./AddUser.css";
+import classes from "./AddUser.module.css";
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 const AddUser = props => {
 
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(event.target[0].value);
     }
 
     return (
-        <Card>
-            <form onSubmit={addUserHandler} className="adduser">
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
                 <input id="username" type="text"></input>
                 <label htmlFor="age">Age(Years)</label>
                 <input id="age" type="number"></input>
-                <button type="submit">Add User</button>
+                <Button type="submit">Add User</Button>
             </form>
         </Card>
     )
